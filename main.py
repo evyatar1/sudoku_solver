281c9139d7a341b53572f22eff6c2ccc71d86307
Evyatar Hen <evyatar037@gmail.com>
2022-05-23 00:42:33 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 board = [
     [0, 2, 0, 3, 0, 0, 0, 0, 0],
